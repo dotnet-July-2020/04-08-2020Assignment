cb69921e65094a2e9b80a3c483665cf2c540d33e
Geethika <geethika.duddupudi@zensar.com>
2020-08-04 22:31:08 +0530

    Creating sorted list and performing operations on it

---
+++ SortedListInCollections/SortedListInCollections/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortedListInCollections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedList<int, string> sort = new SortedList<int, string>();
+            sort.Add(1, "one");
+            sort.Add(2, "two");
+            sort.Add(3, "three");
+            sort.Add(4, "Four");
+            sort.Add(5, "five");
+            sort.Add(6, "six");
+            sort.Add(7, "seven");
+            foreach(var i in sort)
+            {
+                Console.WriteLine("{0}={1}",i.Key,i.Value);
+            }
+            Console.WriteLine( sort.Count());
+            Console.WriteLine(sort.ContainsKey(8));
+            Console.WriteLine(sort.ContainsValue("eight"));
+            
+            Console.Read();
+          
+        }
+    }
+}
