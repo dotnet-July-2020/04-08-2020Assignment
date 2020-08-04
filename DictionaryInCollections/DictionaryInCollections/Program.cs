f537ec24d0d5ee6c26d073481b6e07e55cbd163a
Geethika <geethika.duddupudi@zensar.com>
2020-08-04 22:32:35 +0530

    Creating dictionary and performing operations on it

---
+++ DictionaryInCollections/DictionaryInCollections/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DictionaryInCollections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> di = new Dictionary<string, string>();
+            di.Add("one", "first");
+            di.Add("two", "second");
+            di.Add("three", "third");
+            di.Add("four", "fourth");
+            di.Add("five", "fifth");
+            foreach(var i in di)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine(di.ContainsKey("one"));
+            Console.WriteLine(di.ContainsValue("second"));
+            Dictionary<string, string>.KeyCollection keyColl = di.Keys;
+            foreach(var j in keyColl)
+            {
+                Console.WriteLine(j);
+            }
+          
+            Console.Read();
+        }
+    }
+}
